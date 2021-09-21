e38ae8307aaac8a25fac29e7c456577defa40523
Yash Khandelwal <65719488+yash255@users.noreply.github.com>
2021-09-21 21:50:14 +0530

    Update ConnectionProvider.java

--- JDBC/src/ConnectionProvider.java
+++ JDBC/src/ConnectionProvider.java
@@ -6,7 +6,7 @@ public class ConnectionProvider {
 		try {
 			if(con==null) {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/test","yash","Yashmysql@25");
+			con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/test","user","password");
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
